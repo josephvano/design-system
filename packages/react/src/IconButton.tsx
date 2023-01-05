c2162959d4866c3f197bf6fb6e5d286d187e0251
Joseph Vano <joseph.vano@gmail.com>
2023-01-05 16:15:04 -0500

    feat(react): add Button and IconButton components

---
+++ packages/react/src/IconButton.tsx
@@ -0,0 +1,26 @@
+import React, {forwardRef} from 'react'
+
+interface IconButtonProps extends React.ComponentProps<'button'> {
+  /** If button is in disabled state **/
+  disabled?: boolean;
+  /** Loading state **/
+  loading?: boolean;
+
+  /** Aria title should be mandatory for icon buttons **/
+  'aria-label'?: string;
+}
+
+const IconButton = forwardRef<HTMLButtonElement, IconButtonProps>(
+  ({disabled, loading, ...rest}, ref) => {
+    return (
+      <button ref={ref} {...rest} disabled={disabled || loading}>
+        {rest.children}
+      </button>
+    )
+  }
+);
+
+IconButton.displayName = "Button";
+
+
+export default IconButton;
