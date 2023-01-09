f99169a7415390a3e6d5a089e0d245d2873437f9
Joseph Vano <joseph.vano@gmail.com>
2023-01-09 16:08:51 -0500

    feat(react): style button

--- packages/react/src/Button.tsx
+++ packages/react/src/Button.tsx
@@ -1,18 +1,47 @@
 import { forwardRef } from 'react';
+import { tokens } from '@jvano-ds/foundation';
+import styled from 'styled-components';
 
 export interface ButtonProps extends React.ComponentProps<'button'> {
   /** If button is in disabled state */
   disabled?: boolean;
   /** Loading state */
   loading?: boolean;
-
-  /** Sweetness */
-  name: string;
+  /** Color based on the color props */
+  color: keyof typeof tokens.colors;
 }
 
+const ButtonStyled = styled.button<ButtonProps>`
+  /* Static styles */
+  all: unset;
+  cursor: pointer;
+  padding: 8px 20px;
+  &:disabled {
+    opacity: 40%;
+  }
+  /* Inherit from design tokens */
+  transition: ${tokens.animations.default.value};
+  color: ${tokens.colors.neutral.white.value};
+  border-radius: ${tokens.radius.large.value};
+  background-color: ${(props) => tokens.colors[props.color][500].value};
+  &:hover {
+    background-color: ${(props) => tokens.colors[props.color][700].value};
+  }
+  &:active {
+    background-color: ${(props) => tokens.colors[props.color][800].value};
+  }
+`;
+
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ name, disabled, loading, color = 'primary', ...rest }, ref) => {
-    return <button {...rest} ref={ref} disabled={disabled || loading} />;
+  ({ disabled, loading, color = 'primary', ...rest }, ref) => {
+    return (
+      <ButtonStyled
+        {...rest}
+        ref={ref}
+        color={color}
+        disabled={disabled || loading}
+      />
+    );
   }
 );
 
